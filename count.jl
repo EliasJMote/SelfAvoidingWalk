f9502c1238ae27824ab4e319c4e426e7f3e09f4b
Ryan Moeller <ryan@freqlabs.com>
2018-12-02 21:29:29 -0800

    Add Julia implementation of counting
    count.jl reads a JSON description of a DFA from stdin, calculates the
    number of strings in the language accepted by the DFA for a range of
    string lengths, and prints each count on a separate line to stdout.
    
    The range of lengths is given by the program's arguments:
    
    1 arg: [arg1,arg1]
    2 args: [arg1,arg2)
    otherwise: [0,infinity)
    
    In the last mode, the program will exit gracefully when stdout is closed.
    
    An example invocation:
    
    ```
    ./count.jl < exampleDFA.json | head -100 | tail -1
    ```

---
+++ count.jl
@@ -0,0 +1,39 @@
+#! /usr/bin/env julia
+
+using JSON
+
+const dfa = JSON.parse(stdin)
+
+const Q = dfa["states"]
+const M = dfa["transitions"]
+const s = dfa["start"]
+const F = Set(dfa["final"])
+
+const m = length(M)
+
+const u = transpose([Int(q == s) for q in Q])
+const v = [Int(q in F) for q in Q]
+const A = [BigInt(count(x -> x == q, row)) for row in M, q in Q]
+
+function f(a, n)
+    println(u * a * v)
+    return a * A
+end
+
+if length(ARGS) == 1
+    n = parse(Int, ARGS[1])
+    println(u * A^n * v)
+elseif length(ARGS) == 2
+    left = parse(Int, ARGS[1])
+    right = parse(Int, ARGS[2])
+    foldl(f, left:right, init=A^left)
+else
+    try
+        foldl(f, Iterators.countfrom(0), init=A)
+    catch e
+        # gracefully exit on EPIPE when stdout closes
+        if typeof(e) != Base.IOError || e.code != Base.UV_EPIPE
+            reraise(e)
+        end
+    end
+end
