5788ea83a48ec8fede20792e04babde03257e9c3
Elias James Mote <eliasjmote@gmail.com>
2018-12-13 10:51:33 -0800

    Initial build of algorithm to create the dfa and set of treated states

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ def main():
 	import sys
 
 	# The total length of the self-avoiding walk we are checking
-	length = 1
+	k = 4
 
-	# The four directions (the language of the DFA) are: right, up, left, down
-	directions = ["r", "u", "l", "d"]
+	# The four directions (the language of the DFA).
+	# 1: Immediate return
+	# 2: Continue in the same direction as the previous step
+	# 3: A step in the opposite direction to the second to last step (if there
+	# isn't a second to last step, this could be considered a left or right)
+	# 4: The remaining direction
+	directions = [1, 2, 3, 4]
 
 	# The start state is 0
 	start_state = 0
@@ -26,25 +31,75 @@ def main():
 	treated = []
 
 	# Initialize an empty set of transfers
-	transfers = []
+	transfers = [[-1,-1,-1,-1] for _ in range(2)]
+
+	# State lengths
+	state_lengths = [0 for _ in range(4)]
 
 	# The current walk length we are considering
-	#cur_length = 1
+	cur_length = 0
 
 	# Repeat steps 2-4 until the set of untreated states is empty
+	# State "w" is a loop and therefore fail state
 	while(len(untreated) > 0):
 
 		# Step 2:
 		# Choose any untreated state s, remove it from the set. This will be
-		# accomplished by popping the last element.
-		s = untreated.pop()
+		# accomplished by removing the first element.
+		s = untreated.pop(0)
+
+		cur_length = state_lengths[s]
+
+		#print("Current state = " + str(s))
+		#print("State length = " + str(cur_length))
+		#print()
+
 
 		# Step 3:
 		# Construct all possible successors of the state by iterating through
 		# all 4 possible directions. In each iteration, augment r by a single
 		# step in the corresponding direction, leading to an augmented walk a.
+		# For l > 1, direction 1 always goes to state "w"
+
+		if(s == 0):
+			untreated.append(1)
+			state_lengths[1] = 1
+		elif(s == 1):
+			untreated.append(2)
+			untreated.append(3)
+			state_lengths[2] = 2
+			state_lengths[3] = 2
+
 		for d in range(4):
 
+			# Initialize state 0
+			# All 4 directions go to state 1
+			if(s == 0):
+				transfers[s][d] = 1
+				
+
+			# Initialize state 1
+			elif(s == 1):
+				if(d == 0):
+					transfers[s][d] = "w"
+				elif(d == 1):
+					transfers[s][d] = 2
+				else:
+					transfers[s][d] = 3
+				
+
+			elif(cur_length < k):
+				if(d == 0):
+					transfers.append([-1,-1,-1,-1])
+					transfers[s][0] = "w"
+				else:
+					# If t is a state we have not seen so far, put it in the set of
+					# untreated states
+					t = max(untreated) + 1
+					untreated.append(t)
+					transfers[s][d] = t
+					state_lengths.append(-1)
+					state_lengths[t] = cur_length + 1
 
 			# If t is a state we have not seen so far, put it in the set of
 			# untreated states
@@ -55,9 +110,11 @@ def main():
 
 
 		# Step 4:
-		# Put state s into the set of treated states. If the set of untreated states
-		# is not empty, go to 2; otherwise, continue to 5
+		# Put state s into the set of treated states. If the set of untreated
+		# states is not empty, go to 2; otherwise, continue to 5
 		treated.append(s)
+
+	transfers.append(["w","w","w","w"])
 
 	# Step 5:
 	# We have now collected all necessary states in the set of treated states
@@ -69,29 +126,34 @@ def main():
 	"""
 	{
 		"transitions":	[
-							[1,1,1,1],
-							[2,2,2,2],
-							[3,3,3,3],
-							[4,4,4,4],
-							[4,4,4,4]
+							["1","1","1","1"],
+							["w","1","2","2"],
+							["w","1","3","2"],
+							["w","1","2","2"],
+							["w","w","w","w"]
 						],
-		"states":[0,1,2,3,4],
-		"final":[4],
-		"symbols":["a","b","c","d"],
-		"start":0
+		"states": [0,1,2,3,4],
+		"final": [3],
+		"symbols": ["f","l","b","r"],
+		"start": 0
 	}
 	"""
-
 	json_dfa = 	{
 					"transitions": transfers,
 					"states": treated,
 					"final": end_states,
-					"symbols": ["r", "u", "l", "d"],
+					"symbols": [1,2,3,4],
 					"start": start_state
 				}
-	sys.stdout.write(str(json_dfa))
-	sys.stdout.flush()
-	print()
+
+	print("Set of treated states:")
+	print(treated)
+	print("Set of transfers:")
+	for t in transfers:
+		print(t)
+	#sys.stdout.write(str(json_dfa))
+	#sys.stdout.flush()
+	#print()
 
 if __name__ == '__main__':
 	main()
